8ae60e3451fe646275e2a586f1619384c081add1
devamcodes <devamagrawal.codes@gmail.com>
2020-07-11 18:32:11 +0530

    set to list converted successfullygit status

---
+++ set_to_list/set_to_list.py
@@ -0,0 +1,33 @@
+"""
+Project:set to list converter
+
+Author:Devam A
+Date:11 july 2020
+
+Descreption:
+This program contains function that converts set into the list
+Status:Tested
+"""
+print("\t\t\t\tset generator\n\n\n")
+def generate_set():
+    output_set=set()
+    form_set = 1
+    while(form_set != 0):
+        set_elements = input("PLEASE ENTER SET ELEMENT:")
+        output_set.add(set_elements)
+        form_set = int(input("PLEASE ENTER 0 TO FORM SET OR ENTER ANY VALUE TO CONTINUE ADDING:"))
+    print(output_set)
+    return output_set
+
+def convert_set_to_list(set):
+    output_list = list()
+    for elements in set:
+        output_list.append(elements)
+    else:
+        print("set converted!")
+    print(output_list)
+    return output_list
+
+my_set = {}
+my_set = generate_set()
+my_list = convert_set_to_list(my_set)

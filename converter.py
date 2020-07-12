688815f407d6ea83c69137b714ec4ed17d05175c
devamcodes <devamagrawal.codes@gmail.com>
2020-07-12 08:39:59 +0530

    git ignore added

---
+++ converter.py
@@ -0,0 +1,28 @@
+"""
+#This converter is for converting the sets into lists only
+friends = set()
+user= input("ENTER YOUR FRIEND'S NAME:")
+#The user function will get the value from user and then it will add it to set 'friends'
+while(user != 0):
+    user_friend = input("ENTER YOUR FRIEND'S NAME:")
+    user_input = input("DO YOU WANT TO CONTINUE??:")
+    user = user_input == "yes"
+#Here the while loop will continue printing this input code till the user value will be false
+#It enable the user to add as many names as needed.   
+my_list = list(friends)
+print(my_list)
+my_list = list[]
+my_list.append(user)
+user = input("enter your name:")
+for elements in user:
+    friend = set()
+    user.add(friend)
+"""
+def generate_list
+    output = []
+    var = 1
+    while (var != 0):
+        user = input("please enter any name:")
+        var = int(input("enter 1 or 0:"))
+        output.append(user)
+    print(output)
---
+++ converter0.py
@@ -0,0 +1,6 @@
+my_list = list[]
+my_list.append(user)
+user = input("enter your name:")
+for elements in user:
+    friend = set()
+    user.add(friend)
